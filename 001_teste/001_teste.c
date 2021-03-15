b3f282592454c1bdfa5722c2349159887a47c723
codingscode <heronnestg@hotmail.com>
2021-03-15 10:51:16 -0300

    inicio

---
+++ 001_teste/001_teste.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    printf("Isto é um teste!");
+ 
+    int a;
+    a = 2 + 7;
+    printf("Valor da Variável: %d\n", a);
+
+    int dinheiro = 3;
+    printf("Quantidade de dinheiro %d", dinheiro); 
+  
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
