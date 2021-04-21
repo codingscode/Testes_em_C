d931c290c93bd50bfe6baf5e4fe57f5946c361cb
codingscode <heronnestg@hotmail.com>
2021-04-21 11:40:37 -0300

    parte 10

---
+++ 001_teste/022_teste.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+ 
+struct Livros {
+   char titulo[50];
+   char autor[50];
+   char assunto[100];
+   int livro_id;
+};
+
+/* declarando função */
+void imprimirLivro( struct Livros *livro );
+
+int main() {
+
+   struct Livros Livro1;        /* declarar Livro1 do tipo Livro */
+   struct Livros Livro2;        /* declarar Livro2 do tipo Livro */
+ 
+   /* livro 1 especificação */
+   strcpy( Livro1.titulo, "Programação em Python");
+   strcpy( Livro1.autor, "Bob Allan"); 
+   strcpy( Livro1.assunto, "Programação em Python Tutorial");
+   Livro1.livro_id = 6495407;
+
+   /* livro 2 especificação */
+   strcpy( Livro2.titulo, "Culinaria economica");
+   strcpy( Livro2.autor, "Frederico Gomes");
+   strcpy( Livro2.assunto, "Culinaria economica Tutorial");
+   Livro2.livro_id = 6495700;
+ 
+   /* imprimir Livro1 info passando endereço de Livro1 */
+   imprimirLivro( &Livro1 );
+
+   /* imprimir Livro2 info passando endereço de Livro2 */
+   imprimirLivro( &Livro2 );
+
+   return 0;
+}
+
+void imprimirLivro( struct Livros *livro ) {
+
+   printf( "Livro titulo : %s\n", livro->titulo);
+   printf( "Livro autor : %s\n", livro->autor);
+   printf( "Livro assunto : %s\n", livro->assunto);
+   printf( "Livro livro_id : %d\n", livro->livro_id);
+}
+
+/* 
+Livro titulo : Programação em Python
+Livro autor : Bob Allan
+Livro assunto : Programação em Python Tutorial
+Livro livro_id : 6495407
+Livro titulo : Culinaria economica
+Livro autor : Frederico Gomes
+Livro assunto : Culinaria economica Tutorial
+Livro livro_id : 6495700
+*/
