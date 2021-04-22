7d94a41a7a96e1f369e47cf8c32de92cd3f00bf9
codingscode <heronnestg@hotmail.com>
2021-04-22 10:06:03 -0300

    parte 11

--- 001_teste/023_teste.c
+++ 001_teste/023_teste.c
@@ -1,12 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+ 
+union Dado {
+   int i;
+   float f;
+   char str[20];
+};
+ 
+int main( ) {
 
+   union Dado dado;        
 
+   printf( "Tamanho de memória ocupado por dado : %d\n", sizeof(dado));
 
+   return 0;
+}
 
-
-
-
-
-
-
-
-
+/* 
+Tamanho de memória ocupado por dado : 20
+ */
---
+++ 001_teste/024_teste.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+ 
+union Dado {
+   int i;
+   float f;
+   char string[20];
+};
+ 
+int main( ) {
+
+   union Dado dado;        
+
+   dado.i = 10;
+   dado.f = 220.5;
+   strcpy( dado.string, "Programação em C");
+
+   printf( "dado.i : %d\n", dado.i);
+   printf( "dado.f : %f\n", dado.f);
+   printf( "dado.string : %s\n", dado.string);
+
+   return 0;
+}
+
+/* 
+dado.i : 1735357008
+dado.f : 1130754282837771129192448.000000
+dado.string : Programação em C
+*/
