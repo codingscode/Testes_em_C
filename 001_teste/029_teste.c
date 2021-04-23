98e46dbdaecc833ca41400285e79da5b6c1d40b1
codingscode <heronnestg@hotmail.com>
2021-04-23 09:23:40 -0300

    parte 15

---
+++ 001_teste/029_teste.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+#define TRUE  1
+#define FALSE 0
+ 
+int main( ) {
+   printf( "Valor de TRUE : %d\n", TRUE);
+   printf( "Valor de FALSE : %d\n", FALSE);
+
+   return 0;
+}
+
+/* 
+Valor de TRUE : 1
+Valor de FALSE : 0
+
+ */
---
+++ 001_teste/030_teste.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main( ) {
+
+   int c;
+
+   printf( "Digite um valor :");
+   c = getchar( );
+
+   printf( "\nVoce digitou: ");
+   putchar( c );
+
+   return 0;
+}
+
+/* 
+Digite um valor :3
+Voce digitou: 3
+*/
