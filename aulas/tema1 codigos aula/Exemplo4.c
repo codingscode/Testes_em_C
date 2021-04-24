59fc8756f68769453a7bd476c64b90483ce354ea
codingscode <heronnestg@hotmail.com>
2021-04-23 22:31:01 -0300

    parte 18

---
+++ aulas/tema1 codigos aula/Exemplo4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void) {
+      //v_num é a variável que será apontada pelo ponteiro
+      int v_num = 10;
+      
+      //declaração de variável ponteiro
+      int *ptr;
+      
+      //atribuindo o endereço da variável v_num ao ponteiro
+      ptr = &v_num;
+      
+      printf("Utilizando ponteiros\n\n");
+      printf ("Conteudo da variavel v_num: %d\n", v_num);
+      printf ("Endereço da variavel v_num: %x \n", &v_num);
+      printf ("Conteudo da variavel ponteiro ptr: %x", ptr);
+      
+      getch();
+      return(0);
+}
+
+/*
+Utilizando ponteiros
+
+Conteudo da variavel v_num: 10
+Endereþo da variavel v_num: 62fe14
+Conteudo da variavel ponteiro ptr: 62fe14
+
+*/
+
