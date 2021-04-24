6df07649cbcae800597ccc0b258fbd59b2ea4a45
codingscode <heronnestg@hotmail.com>
2021-04-24 10:55:14 -0300

    parte 21

---
+++ 001_teste/040_teste.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main() {
+
+   int soma = 17, contar = 5;
+   double media;
+
+   media = (double) soma / contar;
+   printf("Valor da media : %f\n", media );
+}
+
+/* 
+Valor da media : 3.400000
+*/
---
+++ 001_teste/041_teste.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main() {
+
+   int  i = 17;
+   char c = 'c'; /* ascii value is 99 */
+   int soma;
+
+   soma = i + c;
+   printf("Valor de soma : %d\n", soma );
+}
+
+/* 
+Valor de soma : 116
+*/
