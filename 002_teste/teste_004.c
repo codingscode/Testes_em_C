1beb0e8d384c95b0b7b05f34e960d30f73051caa
codingscode <heronnestg@hotmail.com>
2021-04-29 10:41:27 -0300

    parte 39

--- 002_teste/teste_004.c
+++ 002_teste/teste_004.c
@@ -4,13 +4,21 @@
 int main() {
     float numero1 = 13.5;
     double numero2 = 12.4;
+    int numero3 = 3;
+    char caractere = 'a';    
+
 
     printf("numero1 = %f\n", numero1);
-    printf("numero2 = %lf", numero2);
+    printf("numero2 = %lf\n", numero2);
+    printf("numero3 = %d\n", numero2);
+    printf("caractere = %c", caractere);  
+
     return 0;
 }
 
 /*
 numero1 = 13.500000
 numero2 = 12.400000
+numero3 = -858993459
+caractere = a
 */
---
+++ 002_teste/teste_005.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int main() {
+    int testar_inteiro;
+    float numero_float;
+    double numero_double;
+    char caractere;
+
+
+    printf("Digite um caractere: \n"); 
+    scanf("%c", &caractere);     
+    printf("Digite um inteiro: \n");
+    scanf("%d", &testar_inteiro);  
+    printf("Digite um numero float: \n");
+    scanf("%f", &numero_float);  
+    printf("Digite um numero double: \n");
+    scanf("%lf", &numero_double); 
+
+
+
+    printf("Numero inteiro = %d\n", testar_inteiro);
+    printf("Numero float = %f\n", numero_float);
+    printf("Numero double = %lf\n", numero_double);
+    printf("Caractere digitado = %c.\n", caractere);  
+    printf("Caractere digitado valor em ascii = %d.\n", caractere);  
+
+    return 0;
+}
+
+/*
+Digite um caractere:
+k
+Digite um inteiro:
+5
+Digite um numero float:
+1.73
+Digite um numero double:
+6.923
+Numero inteiro = 5
+Numero float = 1.730000
+Numero double = 6.923000
+Caractere digitado = k.
+Caractere digitado valor em ascii = 107.
+*/
