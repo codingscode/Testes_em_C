0b1945d6828b3f983feb4f3320903c79758c44cc
codingscode <heronnestg@hotmail.com>
2021-05-01 22:09:13 -0300

    parte 46

--- 002_teste/teste_016.c
+++ 002_teste/teste_016.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
 
 
+int main() {
 
+    int numero;
+    printf("Digite um inteiro:\n");
+    scanf("%d", &numero);
+
+    // True se o resto for 0
+    if  (numero%2 == 0) {
+        printf("%d eh um inteiro par.",numero);
+    }
+    else {
+        printf("%d eh um inteiro impar.",numero);
+    }
+
+    return 0;
+}
+
+/*
+Digite um inteiro:
+15
+15 eh um inteiro impar.
+*/
