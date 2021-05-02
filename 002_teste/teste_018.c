56ac3208907e93e82a15b89b7c63809c4f2270a5
codingscode <heronnestg@hotmail.com>
2021-05-02 19:40:05 -0300

    parte 48

--- 002_teste/teste_018.c
+++ 002_teste/teste_018.c
@@ -1,5 +1,29 @@
+#include <stdio.h>
 
 
+int main() {
+    int numero1, numero2;
+    printf("Digite dois numeros um abaixo do outro: \n");
+    scanf("%d %d", &numero1, &numero2);
 
+    if (numero1 >= numero2) {
+      if (numero1 == numero2) {
+        printf("Resultado: %d = %d",numero1,numero2);
+      }
+      else {
+        printf("Resultado: %d > %d", numero1, numero2);
+      }
+    }
+    else {
+        printf("Resultado: %d < %d",numero1, numero2);
+    }
 
+    return 0;
+}
 
+/*
+Digite dois numeros um abaixo do outro:
+8
+4
+Resultado: 8 > 4
+*/
