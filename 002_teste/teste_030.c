221480fa90a637ce4e3499049020a6f38cdcb6f4
codingscode <heronnestg@hotmail.com>
2021-05-05 16:06:51 -0300

    parte 56

---
+++ 002_teste/teste_030.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main() {
+    double numero1, numero2, numero3;
+    printf("Digite tres numeros um abaixo do outro:\n");
+    scanf("%lf %lf %lf", &numero1, &numero2, &numero3);
+
+    if (numero1 >= numero2) {
+        if (numero1 >= numero3)
+            printf("%.2lf eh o maior numero.", numero1);
+        else
+            printf("%.2lf eh o maior numero.", numero3);
+    } else {
+        if (numero2 >= numero3)
+            printf("%.2lf eh o maior numero.", numero2);
+        else
+            printf("%.2lf eh o maior numero.", numero3);
+    }
+
+    return 0;
+}
+
+/* 
+Digite tres numeros um abaixo do outro:
+8
+50
+16
+50.00 eh o maior numero.
+
+*/
