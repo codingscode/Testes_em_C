0752977cf5db00245d58d1167aa8a0164fc3a92c
codingscode <heronnestg@hotmail.com>
2021-05-09 10:44:06 -0300

    parte 69

---
+++ 002_teste/teste_047.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main() {
+    int numero, rev = 0, resto;
+    printf("Digite um numero inteiro:\n");
+    scanf("%d", &numero);
+    while (numero != 0) {
+        resto = numero % 10;
+        rev = rev * 10 + resto;
+        numero /= 10;
+    }
+    printf("Numero invertido = %d", rev);
+    return 0;
+}
+
+/* 
+Digite um numero inteiro:
+427
+Numero invertido = 724
+*/
