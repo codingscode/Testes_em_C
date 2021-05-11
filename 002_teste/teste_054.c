3f5f82ce3b0c999f61a3d4ef094a397fd21ce6bd
codingscode <heronnestg@hotmail.com>
2021-05-11 09:15:15 -0300

    parte 75

---
+++ 002_teste/teste_054.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+int main() {
+    int numero, numero_original, resto, resultado = 0;
+    printf("Digite um inteiro de 3 digitos:\n");
+    scanf("%d", &numero);
+    numero_original = numero;
+
+    while (numero_original != 0) {
+       // resto contem o ultimo digito
+        resto = numero_original % 10;
+        
+       resultado += resto * resto * resto;
+        
+       // removendo ultimo digito do numero original
+       numero_original /= 10;
+    }
+
+    if (resultado == numero)
+        printf("%d eh um numero de Armstrong.", numero);
+    else
+        printf("%d nao eh um numero de Armstrong.", numero);
+
+    return 0;
+}
+
+/* 
+Digite um inteiro de 3 digitos:
+371
+371 eh um numero de Armstrong.
+
+ou
+
+Digite um inteiro de 3 digitos:
+243
+243 nao eh um numero de Armstrong.
+
+*/
