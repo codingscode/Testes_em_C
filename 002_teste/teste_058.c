4ae20eee64ea0d9f7ce43ef2235e92e7b8c6e79a
codingscode <heronnestg@hotmail.com>
2021-05-12 10:06:05 -0300

    parte 79

---
+++ 002_teste/teste_058.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main() {
+   int indice, indice2, linhas;
+   printf("Digite o numero de linhas:\n");
+   scanf("%d", &linhas);
+   for (indice = 1; indice <= linhas; ++indice) {
+      for (indice2 = 1; indice2 <= indice; ++indice2) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+/* 
+Digite o numero de linhas:
+5
+*
+* *
+* * *
+* * * *
+* * * * *
+*/
