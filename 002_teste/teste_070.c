64a01e31b53a3cec2a57b8b9c30e868e143973a6
codingscode <heronnestg@hotmail.com>
2021-05-13 10:21:50 -0300

    parte 86

---
+++ 002_teste/teste_070.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int adicionarNumeros(int a, int b);         // prototipo de funcao
+
+int main() {
+    int numero1, numero2, soma;
+
+    printf("Digite dois numeros:\n");
+    scanf("%d %d",&numero1, &numero2);
+
+    soma = adicionarNumeros(numero1, numero2);        // chama funcao
+    printf("soma = %d",soma);
+
+    return 0;
+}
+
+int adicionarNumeros(int a, int b) {        // definicao de funcao
+
+    int resultado;
+    resultado = a + b;
+    return resultado;                  // retorno
+}
+
+/*
+Digite dois numeros:
+6
+7
+soma = 13
+*/
