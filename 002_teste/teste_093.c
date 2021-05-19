0b7624c5baeb6f8c8f50ce2b78f3795ca17b6489
codingscode <heronnestg@hotmail.com>
2021-05-19 09:52:55 -0300

    parte 106

---
+++ 002_teste/teste_093.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+  int valores[5];
+  
+
+  printf("Digite 5 inteiros:\n");
+
+  // recebendo entrada e armazenando-a em um array
+  for (int indice = 0; indice < 5; ++indice) {
+     scanf("%d", &valores[indice]);
+  }
+
+  printf("Mostrando inteiros:\n");
+
+  // imprimindo elementos de um array
+  for (int indice2 = 0; indice2 < 5; ++indice2) {
+     printf("%d\n", valores[indice2]);
+  }
+  return 0;
+}
+
+/* 
+Digite 5 inteiros:
+3
+5
+1
+10
+6
+Mostrando inteiros:
+3
+5
+1
+10
+6
+*/
