1fe9bd1686dd5075d945a069a0c5341924a87aa8
codingscode <heronnestg@hotmail.com>
2021-05-19 10:59:32 -0300

    parte 108

---
+++ 002_teste/teste_095.c
@@ -0,0 +1,62 @@
+// C program to store temperatura of two cities of a week and display it.
+#include <stdio.h>
+
+
+const int CIDADE = 2;
+const int SEMANA = 7;
+
+int main() {
+  int temperatura[CIDADE][SEMANA];
+
+  // Using nested loop to store values in a 2d array
+  for (int indice1 = 0; indice1 < CIDADE; ++indice1) {
+    for (int indice2 = 0; indice2 < SEMANA; ++indice2) {
+      printf("Cidade %d, Dia %d: ", indice1 + 1, indice2 + 1);
+      scanf("%d", &temperatura[indice1][indice2]);
+    }
+  }
+
+  printf("\nMostrando valores:\n\n");
+
+  // Using nested loop to display vlues of a 2d array
+  for (int indice3 = 0; indice3 < CIDADE; ++indice3) {
+    for (int indice4 = 0; indice4 < SEMANA; ++indice4) {
+      printf("Cidade %d, Dia %d = %d\n", indice3 + 1, indice4 + 1, temperatura[indice3][indice4]);
+    }
+  }
+  return 0;
+}
+
+/* 
+Cidade 1, Dia 1: 40
+Cidade 1, Dia 2: 30
+Cidade 1, Dia 3: 27
+Cidade 1, Dia 4: 28
+Cidade 1, Dia 5: 28
+Cidade 1, Dia 6: 28
+Cidade 1, Dia 7: 30
+Cidade 2, Dia 1: 21
+Cidade 2, Dia 2: 18
+Cidade 2, Dia 3: 18
+Cidade 2, Dia 4: 18
+Cidade 2, Dia 5: 22
+Cidade 2, Dia 6: 21
+Cidade 2, Dia 7: 30
+
+Mostrando valores:
+
+Cidade 1, Dia 1 = 40
+Cidade 1, Dia 2 = 30
+Cidade 1, Dia 3 = 27
+Cidade 1, Dia 4 = 28
+Cidade 1, Dia 5 = 28
+Cidade 1, Dia 6 = 28
+Cidade 1, Dia 7 = 30
+Cidade 2, Dia 1 = 21
+Cidade 2, Dia 2 = 18
+Cidade 2, Dia 3 = 18
+Cidade 2, Dia 4 = 18
+Cidade 2, Dia 5 = 22
+Cidade 2, Dia 6 = 21
+Cidade 2, Dia 7 = 30
+*/
