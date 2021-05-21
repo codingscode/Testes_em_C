b21c6d5b0cd5033d3dff671ffbc56008d4bf92df
codingscode <heronnestg@hotmail.com>
2021-05-21 09:50:15 -0300

    parte 115

---
+++ 002_teste/teste_102.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+    int *ponteiro, c;
+    c = 5;
+    ponteiro = &c;
+    printf("*ponteiro: %d\n", *ponteiro);
+    printf("ponteiro: %d\n", ponteiro);
+
+    return (0);
+}
+
+/* 
+*ponteiro: 5
+ponteiro: 6487572
+*/
