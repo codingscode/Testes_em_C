1df37f88895ebee59c8630003712c90dc8a37117
codingscode <heronnestg@hotmail.com>
2021-05-21 10:34:59 -0300

    parte 116

---
+++ 002_teste/teste_103.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+    int *ponteiro, c;
+    c = 5;
+    ponteiro = &c;
+    c = 1;
+    printf("c: %d\n", c);    
+    printf("*ponteiro: %d\n", *ponteiro); 
+
+    return (0);
+}
+
+/* 
+c: 1
+*ponteiro: 1
+*/
---
+++ 002_teste/teste_104.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+    int *ponteiro, c;
+    c = 5;
+    ponteiro = &c;
+    *ponteiro = 1;
+    printf("*ponteiro: %d\n", *ponteiro);
+    printf("c: %d\n", c); 
+
+    return (0);
+}
+
+/* 
+*ponteiro: 1
+c: 1
+*/
