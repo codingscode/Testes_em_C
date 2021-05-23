2dea36bb7a54ef7713a6aeeb41533a9a54ce2292
codingscode <heronnestg@hotmail.com>
2021-05-23 09:49:15 -0300

    parte 124

---
+++ 002_teste/teste_113.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+void adicionarUm(int *ponteiro) {
+    (*ponteiro)++; // adicionando 1  para *ponteiro
+}
+
+int main() {
+    int *p, indice = 10;
+    p = &indice;
+    adicionarUm(p);
+
+    printf("*p : %d", *p); // 11
+    return 0;
+}
+
+/* 
+*p : 11
+*/
