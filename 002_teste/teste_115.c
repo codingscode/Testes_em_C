afcd2066a50e4d1a7d7869cd39a95f112801a1b6
codingscode <heronnestg@hotmail.com>
2021-05-23 10:12:21 -0300

    parte 126

---
+++ 002_teste/teste_115.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int quantidade, indice, *ponteiro, soma = 0;
+
+    printf("Digite quantidade de elementos:\n");
+    scanf("%d", &quantidade);
+
+    ponteiro = (int*) calloc(quantidade, sizeof(int));
+    if (ponteiro == NULL) {
+        printf("Erro! memoria nao alocada.");
+        exit(0);
+    }
+
+    printf("Digite elements:\n");
+    for (indice = 0; indice < quantidade; ++indice) {
+        scanf("%d", ponteiro + indice);
+        soma += *(ponteiro + indice);
+    }
+
+    printf("Soma = %d", soma);
+    free(ponteiro);
+    return 0;
+}
+
+/* 
+Digite quantidade de elementos:
+3
+Digite elements:
+8
+3
+12
+Soma = 23
+*/
