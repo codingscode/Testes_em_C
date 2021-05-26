1413e3e57f1d2cde254d829afc300e8fa17f62ad
codingscode <heronnestg@hotmail.com>
2021-05-26 09:48:13 -0300

    parte 135

---
+++ 002_teste/teste_123.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main() {
+    int dados[5];
+
+    printf("Digite 5 elementos um abaixo do outro:\n");
+    for (int i = 0; i < 5; ++i)
+        scanf("%d", dados + i);
+
+    printf("Voce digitou:\n");
+    for (int i = 0; i < 5; ++i)
+        printf("%d\n", *(dados + i));
+    return 0;
+}
+
+/* 
+Digite 5 elementos um abaixo do outro:
+5
+2
+10
+1
+8
+Voce digitou:
+5
+2
+10
+1
+8
+*/
