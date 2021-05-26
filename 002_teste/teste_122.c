be4727cc220487efa05f0b61fa2f5c7fce2acc2a
codingscode <heronnestg@hotmail.com>
2021-05-26 09:42:42 -0300

    parte 134

--- 002_teste/teste_122.c
+++ 002_teste/teste_122.c
@@ -2,44 +2,63 @@
 
 
 int main() {
-  int a[10][10], transpose[10][10], r, c;
-  printf("Enter rows and columns: ");
-  scanf("%d %d", &r, &c);
+  int matriz[10][10], transposta[10][10], linha, coluna;
+  printf("Digite linhas e colunas:\n");
+  scanf("%d %d", &linha, &coluna);
 
-  // asssigning elements to the matrix
-  printf("\nEnter matrix elements:\n");
-  for (int i = 0; i < r; ++i)
-    for (int j = 0; j < c; ++j) {
-        printf("Enter element a%d%d: ", i + 1, j + 1);
-        scanf("%d", &a[i][j]);
+  // atribuindo elementos a matriz
+  printf("\nDigite elementos da matriz:\n");
+  for (int i = 0; i < linha; ++i)
+    for (int j = 0; j < coluna; ++j) {
+        printf("Digite element matriz(%dx%d): ", i + 1, j + 1);
+        scanf("%d", &matriz[i][j]);
     }
 
-  // printing the matrix a[][]
-  printf("\nEntered matrix: \n");
-  for (int i = 0; i < r; ++i)
-  for (int j = 0; j < c; ++j) {
-    printf("%d  ", a[i][j]);
-    if (j == c - 1)
-    printf("\n");
-  }
+  // imprimindo a matriz
+  printf("\nMatriz digitada:\n");
+  for (int i = 0; i < linha; ++i)
+    for (int j = 0; j < coluna; ++j) {
+      printf("%d  ", matriz[i][j]);
+      if (j == coluna - 1)
+        printf("\n");
+    }
 
-  // computing the transpose
-  for (int i = 0; i < r; ++i)
-  for (int j = 0; j < c; ++j) {
-    transpose[j][i] = a[i][j];
-  }
+  // computando a transposta
+  for (int i = 0; i < linha; ++i)
+    for (int j = 0; j < coluna; ++j) {
+      transposta[j][i] = matriz[i][j];
+    }
 
-  // printing the transpose
-  printf("\nTranspose of the matrix:\n");
-  for (int i = 0; i < c; ++i)
-  for (int j = 0; j < r; ++j) {
-    printf("%d  ", transpose[i][j]);
-    if (j == r - 1)
-    printf("\n");
-  }
+  // imprimindo a transposta
+  printf("\nTransposta da matriz:\n");
+  for (int i = 0; i < coluna; ++i)
+    for (int j = 0; j < linha; ++j) {
+      printf("%d  ", transposta[i][j]);
+      if (j == linha - 1)
+        printf("\n");
+    }
   return 0;
 }
 
 /* 
+Digite linhas e colunas:
+2
+3
 
+Digite elementos da matriz:
+Digite element matriz(1x1): 1
+Digite element matriz(1x2): 2
+Digite element matriz(1x3): 3
+Digite element matriz(2x1): 4
+Digite element matriz(2x2): 5
+Digite element matriz(2x3): 6
+
+Matriz digitada:
+1  2  3
+4  5  6
+
+Transposta da matriz:
+1  4
+2  5
+3  6
 */
