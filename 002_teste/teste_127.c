7db1e541467ccdf94060ee2257efd6724396711e
codingscode <heronnestg@hotmail.com>
2021-05-27 11:45:17 -0300

    parte 139

---
+++ 002_teste/teste_127.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+    char nome[30];
+    printf("Digite nome:\n");
+    fgets(nome, sizeof(nome), stdin);  // ler string
+    printf("Nome: ");
+    puts(nome);    // mostra string
+    return 0;
+}
+
+/* 
+Digite nome:
+michael jackson
+Nome: michael jackson
+*/
