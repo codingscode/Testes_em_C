d6e9938c61894dc43b636781d58244721ccc0853
codingscode <heronnestg@hotmail.com>
2021-05-27 11:42:22 -0300

    parte 138

---
+++ 002_teste/teste_126.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+    char nome[20];
+    printf("Digite nome:\n");
+    scanf("%s", nome);
+    printf("Seu nome eh %s.", nome);
+    return 0;
+}
+
+/* 
+Digite nome:
+michael jackson
+Seu nome eh michael.
+*/
