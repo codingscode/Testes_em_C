c8faaa6ec757bddd2b9cec1de4b8329b9f2a6363
codingscode <heronnestg@hotmail.com>
2021-05-31 10:05:11 -0300

    parte 151

---
+++ 002_teste/teste_139.c
@@ -0,0 +1,59 @@
+// Program to add two distances (pe-polegada)
+#include <stdio.h>
+
+
+struct Distancia {
+    int pe;
+    float polegada;
+} dist1, dist2, soma;
+
+
+int main() {
+    printf("Primeira distancia:\n");
+    printf("Digite em pe: ");
+    scanf("%d", &dist1.pe);
+
+    printf("Digite polegada: ");
+    scanf("%f", &dist1.polegada);
+    printf("2nd distance\n");
+
+    printf("Digite pe: ");
+    scanf("%d", &dist2.pe);
+
+    printf("Digite polegada: ");
+    scanf("%f", &dist2.polegada);
+
+    // adding pe
+    soma.pe = dist1.pe + dist2.pe;
+    // adding inches
+    soma.polegada = dist1.polegada + dist2.polegada;
+
+    // changing to pe if polegada is greater than 12
+    while (soma.polegada >= 12) {
+        ++soma.pe;
+        soma.polegada = soma.polegada - 12;
+    }
+
+    printf("Soma de distancias = %d\'-%.1f\"", soma.pe, soma.polegada);
+    return 0;
+}
+
+/* 
+Primeira distancia:
+Digite em pe: 12
+Digite polegada: 7.9
+2nd distance
+Digite pe: 2
+Digite polegada: 9.8
+Soma de distancias = 15'-5.7"
+
+ou
+
+Primeira distancia:
+Digite em pe: 3
+Digite polegada: 8
+2nd distance
+Digite pe: 9
+Digite polegada: 1
+Soma de distancias = 12'-9.0"
+*/
