7e4456022aa7389480d67e3ef405aac6528fb64a
codingscode <heronnestg@hotmail.com>
2021-06-01 08:54:08 -0300

    parte 155

---
+++ 002_teste/teste_143.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+struct estudante {
+    char nome[50];
+    int idade;
+};
+
+// function prototype
+struct estudante acessarInformacao();
+
+int main() {
+    struct estudante s;
+
+    s = acessarInformacao();
+
+    printf("\nMostrando informacao:\n");
+    printf("Nome: %s", s.nome);
+    printf("\nIdade: %d", s.idade);
+    
+    return 0;
+}
+
+struct estudante acessarInformacao() {
+  struct estudante s1;
+
+  printf("Digitar nome:\n");
+  scanf ("%[^\n]%*c", s1.nome);
+
+  printf("Digitar idade:\n");
+  scanf("%d", &s1.idade);
+  
+  return s1;
+}	
+
+/* 
+Digitar nome:
+michael jackson
+Digitar idade:
+80
+
+Mostrando informacao:
+Nome: michael jackson
+Idade: 80
+*/
