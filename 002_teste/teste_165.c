2bd91c2bdacdae593733c30efe8d05f834bca7c2
codingscode <heronnestg@hotmail.com>
2021-06-09 09:38:18 -0300

    parte 178

---
+++ 002_teste/teste_165.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+#define PI 3.1415
+#define areaCirculo(r) (PI*pow(r, 2))
+
+
+int main() {
+    float raio, area;
+
+    printf("Digite o raio:\n");
+    scanf("%f", &raio);
+    area = areaCirculo(raio);
+    printf("Area = %.2f", area);
+
+    return 0;
+}
+
+/* 
+Digite o raio:
+4
+Area = 50.26
+*/
