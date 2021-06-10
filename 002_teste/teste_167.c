ec369196e61aa120f8ede3264a9aa0ad67a66a3e
codingscode <heronnestg@hotmail.com>
2021-06-10 10:15:35 -0300

    parte 181

--- 002_teste/teste_167.c
+++ 002_teste/teste_167.c
@@ -1,3 +1,18 @@
+/* 
+C Header Files
+<assert.h>	Program assertion functions
+<ctype.h>	Character type functions
+<locale.h>	Localization functions
+<math.h>	Mathematics functions
+<setjmp.h>	Jump functions
+<signal.h>	Signal handling functions
+<stdarg.h>	Variable arguments handling functions
+<stdio.h>	Standard Input/Output functions
+<stdlib.h>	Standard Utility functions
+<string.h>	String handling functions
+<time.h>	Date time functions
+*/
+
 #include <stdio.h>
 #include <math.h>
 
---
+++ 002_testes_rev/teste_001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+   printf("Ola pessoas!");
+   return 0;
+}
+
+/* 
+Ola pessoas!
+*/
