38575f2d78719eb9a06a78a7780130c7a0421314
codingscode <heronnestg@hotmail.com>
2021-06-10 10:24:29 -0300

    parte 182

---
+++ 002_testes_rev/teste_002.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {   
+    int numero;
+   
+    printf("Digite um inteiro:\n");  
+    
+    scanf("%d", &numero);
+    printf("Voce digitou:\n%d", numero);
+    
+    return 0;
+}
+
+/* 
+Digite um inteiro:
+8
+Voce digitou:
+8
+*/
