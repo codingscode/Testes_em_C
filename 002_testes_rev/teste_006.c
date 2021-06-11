5b4f5c70a64fe582309831d13ebc4d6021b6cba9
codingscode <heronnestg@hotmail.com>
2021-06-11 12:05:35 -0300

    parte 186

---
+++ 002_testes_rev/teste_006.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main() {
+    int dividendo, divisor, quociente, resto;
+    printf("Digite dividendo:\n");
+    scanf("%d", &dividendo);
+    printf("Digite divisor:\n");
+    scanf("%d", &divisor);
+
+    // computa quociente
+    quociente = dividendo / divisor;
+
+    // computa resto
+    resto = dividendo % divisor;
+
+    printf("Quociente = %d\n", quociente);
+    printf("Resto = %d", resto);
+    return 0;
+}
+
+/* 
+Digite dividendo:
+20
+Digite divisor:
+7
+Quociente = 2
+Resto = 6
+*/
