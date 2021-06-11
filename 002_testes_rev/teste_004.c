60d582da0c59c5a1fdae8abc147566fef2da723f
codingscode <heronnestg@hotmail.com>
2021-06-11 11:28:25 -0300

    parte 184

---
+++ 002_testes_rev/teste_004.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+    double numero1, numero2, produto;
+    printf("Digite dois numeros um abaixo do outro:\n");
+    scanf("%lf %lf", &numero1, &numero2);  
+ 
+    produto = numero1 * numero2;
+
+    printf("Produto = %.3lf", produto);
+    
+    return 0;
+}
+
+/* 
+
+*/
