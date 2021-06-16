db7b576e7d66fb4e6df11d176afa4cce18fdde07
codingscode <heronnestg@hotmail.com>
2021-06-16 11:20:31 -0300

    parte 195

--- 001_teste/007_teste.c
+++ 001_teste/007_teste.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
  
-/* function declaration */
-void func(void);
+/* declaracao de funcao */
+void funcao(void);
  
-static int count = 5; /* global variable */
+static int contador = 5; /* variavel global */
  
 main() {
 
-   while(count--) {
-      func();
+   while (contador--) {
+      funcao();
    }
 	
-   return 0;
+   return (0);
 }
 
-/* function definition */
-void func( void ) {
+/* definicao de funcao */
+void funcao( void ) {
 
-   static int i = 5; /* local static variable */
+   static int i = 5; /* variavel estatica local */
    i++;
 
-   printf("i is %d and count is %d\n", i, count);
+   printf("i eh %d e contador eh %d\n", i, contador);
 }
+
+/*
+i eh 6 e contador eh 4
+i eh 7 e contador eh 3
+i eh 8 e contador eh 2
+i eh 9 e contador eh 1
+i eh 10 e contador eh 0
+*/
+
