416b6e7749aeb4034a672aff108f3d219a656dea
codingscode <heronnestg@hotmail.com>
2021-06-17 10:26:36 -0300

    parte 199

--- 001_teste/014_teste.c
+++ 001_teste/014_teste.c
@@ -6,13 +6,13 @@ int main () {
    int i, j;
  
    /* inicializar elementos da array n em 0 */         
-   for ( i = 0; i < 10; i++ ) {
-      n[ i ] = i + 100; /* set element at location i to i + 100 */
+   for ( i = 0; i < sizeof(n); i++ ) {
+      n[ i ] = i + 100; /* coloque elemento na locacao i a i+100 */
    }
    
-   /* output each array element's value */
+   /* saida de cada valor do elemento array */
    for (j = 0; j < 10; j++ ) {
-      printf("Elemento[%d] = %d\n", j, n[j] );
+      printf("Elemento[%d] = %d\n", j, n[j]);
    }
  
    return 0;
@@ -29,4 +29,4 @@ Elemento[6] = 106
 Elemento[7] = 107
 Elemento[8] = 108
 Elemento[9] = 109
- */
+*/
--- 001_teste/015_teste.c
+++ 001_teste/015_teste.c
@@ -1,19 +1,34 @@
 /* Ponteiros */
-
 #include <stdio.h>
+
 
 int main () {
 
    int var1;
    char var2[10];
+   int var3 = 4;
+   char var4[2] = {"oi"};
 
-   printf("Endereço de variavel var1: %x\n", &var1  );
-   printf("Endereço de variavel var2: %x\n", &var2  );
+   printf("Endereco de variavel var1: %x\n", &var1);
+   printf("Endereco de variavel var2: %x\n", &var2);
+   printf("Endereco de variavel var3: %x\n", &var3);
+   printf("Endereco de variavel var4: %x\n", &var4);
+
+   printf("var1: %d\n", var1);
+   printf("var2: %s\n", var2);
+   printf("var3: %d\n", var3);
+   printf("var4: %s\n", var4);
 
    return 0;
 }
 
 /* 
-Endereço de variavel var1: 3bad2278
-Endereço de variavel var2: 3bad226e
- */
+Endereco de variavel var1: 62fe1c
+Endereco de variavel var2: 62fe10
+Endereco de variavel var3: 62fe0c
+Endereco de variavel var4: 62fe00
+var1: 0
+var2:
+var3: 4
+var4: oi
+*/
