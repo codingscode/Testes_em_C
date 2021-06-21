ba7da9a2cc477360a47c945a2ea925d98ca64521
codingscode <heronnestg@hotmail.com>
2021-06-21 09:58:47 -0300

    parte 210

--- 001_teste/032_teste.c
+++ 001_teste/032_teste.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-int main( ) {
+
+
+int main() {
 
    char str[100];
-   int i;
+   int inteiro;
 
-   printf( "digite 2 valores um abaixo do outro : \n");
-   scanf("%s %d", str, &i);
+   printf("digite 2 valores um abaixo do outro: \n");
+   scanf("%s %d", str, &inteiro);
 
-   printf( "\nVoce digitou: %s %d ", str, i);
+   printf("Voce digitou: %s %d \n", str, inteiro);
 
    return 0;
 }
 
 /* 
-digite 2 valores um abaixo do outro :
-6
-7
-
-Voce digitou: 6 7
+digite 2 valores um abaixo do outro:
+felicidade
+8
+Voce digitou: felicidade 8
 */
--- 001_teste/033_teste.c
+++ 001_teste/033_teste.c
@@ -3,15 +3,20 @@
 */
 #include <stdio.h>
 
+
 main() {
    FILE *fp;
 
    fp = fopen("033_teste_arquivo.txt", "w+");
-   fprintf(fp, "Isto é um teste para fprintf...\n");
-   fputs("Isto Ã© um teste para fputs...\n", fp);
+   fprintf(fp, "Isto eh um teste para fprintf...\n");
+   fputs("Isto eh um teste para fputs...\n", fp);
    fclose(fp);
 }
 
 /* 
+o arquivo 033_teste_arquivo.txt fica :
+
+Isto eh um teste para fprintf...
+Isto eh um teste para fputs...
 
 */
