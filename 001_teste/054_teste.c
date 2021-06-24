ae7994d997124c511d1b80b868577e2012d36686
codingscode <heronnestg@hotmail.com>
2021-06-24 10:18:28 -0300

    parte 218

--- 001_teste/054_teste.c
+++ 001_teste/054_teste.c
@@ -3,20 +3,19 @@
 
 
 int main(void) {
-  //valor é a variável que
-  //será apontada pelo ponteiro
+  //valor eh a variavel que serah apontada pelo ponteiro
   int valor = 27;
   
-  //declaracao de variável ponteiro
+  //declaracao de variavel ponteiro
   int *ptr;
   
-  //atribuindo o endereço da variável valor ao ponteiro
+  //atribuindo o endereco da variavel valor ao ponteiro
   ptr = &valor;
   
   printf("Utilizando ponteiros\n\n");
-  printf ("Conteudo da variavel valor: %d\n", valor);
-  printf ("Endereco da variavel valor: %x \n", &valor);
-  printf ("Conteudo da variavel ponteiro ptr: %x", ptr);
+  printf("Conteudo da variavel valor: %d\n", valor);
+  printf("Endereco da variavel valor: %x \n", &valor);
+  printf("Conteudo da variavel ponteiro ptr: %x", ptr);
   
   getch();
   return(0);
@@ -26,6 +25,6 @@ int main(void) {
 Utilizando ponteiros
 
 Conteudo da variavel valor: 27
-Endere+�o da variavel valor: 62fe14
+Endereco da variavel valor: 62fe14
 Conteudo da variavel ponteiro ptr: 62fe14
 */
--- 001_teste/056_teste.c
+++ 001_teste/056_teste.c
@@ -3,6 +3,7 @@
 #include <limits.h>
 #include <float.h>
 
+
 int main(int argc, char** argv) {
 
     printf("Tamanho armazenagem para float : %d \n", sizeof(float));
@@ -12,7 +13,7 @@ int main(int argc, char** argv) {
     printf("-FLT_MIN    :   %g\n", (float) -FLT_MIN);
     printf("DBL_MAX     :   %g\n", (double) DBL_MAX);
     printf("DBL_MIN     :   %g\n", (double) DBL_MIN);
-    printf("-DBL_MAX     :  %g\n", (double) -DBL_MAX);
+    printf("-DBL_MAX    :   %g\n", (double) -DBL_MAX);
     printf("Precisao de valor: %d\n", FLT_DIG );
 
     return 0;
@@ -26,6 +27,6 @@ FLT_MIN     :   1.17549e-038
 -FLT_MIN    :   -1.17549e-038
 DBL_MAX     :   1.79769e+308
 DBL_MIN     :   2.22507e-308
--DBL_MAX     :  -1.79769e+308
+-DBL_MAX    :   -1.79769e+308
 Precisao de valor: 6
 */
