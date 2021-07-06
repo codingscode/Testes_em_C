66089d9dc1d444bfce95bf8b826e15d548942d82
codingscode <heronnestg@hotmail.com>
2021-07-06 11:00:05 -0300

    parte 253

--- 002_teste/teste_090.c
+++ 002_teste/teste_090.c
@@ -11,21 +11,19 @@ int main() {
     scanf("%d", &numero_octal);
 
     printf("%d de octal = %lld para decimal", numero_octal, converterOctalparaDecimal(numero_octal));
-
     return 0;
 }
 
 long long converterOctalparaDecimal(int num_oct) {
-    int numero_decimal = 0, indice = 0;
+    int numero_decimal = 0, expoente = 0;
 
     while (num_oct != 0) {
-        numero_decimal += (num_oct%10) * pow(8,indice);
-        ++indice;
+        numero_decimal += (num_oct % 10) * pow(8, expoente);
+        ++expoente;
         num_oct /= 10;
     }
 
-    indice = 1;
-
+    expoente = 1;
     return numero_decimal;
 }
 
@@ -40,4 +38,9 @@ Digite um numero octal:
 23
 23 de octal = 19 para decimal
 
+ou
+
+Digite um numero octal:
+275
+275 de octal = 189 para decimal
 */
--- 002_teste/teste_091.c
+++ 002_teste/teste_091.c
@@ -13,21 +13,22 @@ int main() {
 }
 
 int converter(long long bin) {
-    int octal = 0, decimal = 0, indice = 0;
+    int octal = 0, decimal = 0, expoente = 0;
 
     // convertendo binario para decimal
     while (bin != 0) {
-        decimal += (bin % 10) * pow(2, indice);
-        ++indice;
+        decimal += (bin % 10) * pow(2, expoente);
+        ++expoente;
         bin /= 10;
+        printf("decimal -> %d\n", decimal);
     }
-    indice = 1;
+    expoente = 1;
 
     // convertendo decimal para octal
     while (decimal != 0) {
-        octal += (decimal % 8) * indice;
+        octal += (decimal % 8) * expoente;
         decimal /= 8;
-        indice *= 10;
+        expoente *= 10;
     }
     return octal;
 }
@@ -35,11 +36,31 @@ int converter(long long bin) {
 /* 
 Digite um numero binario:
 11011
+decimal -> 1
+decimal -> 3
+decimal -> 3
+decimal -> 11
+decimal -> 27
 11011 de binario = 33 para octal
 
 ou
 
 Digite um numero binario:
 111
+decimal -> 1
+decimal -> 3
+decimal -> 7
 111 de binario = 7 para octal
+
+ou
+
+Digite um numero binario:
+111001
+decimal -> 1
+decimal -> 1
+decimal -> 1
+decimal -> 9
+decimal -> 25
+decimal -> 57
+111001 de binario = 71 para octal
 */
