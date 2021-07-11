fb4c41bd52b2dcef453c99ed34adcd9327443703
codingscode <heronnestg@hotmail.com>
2021-07-11 09:44:48 -0300

    parte 267

--- 002_teste/teste_118.c
+++ 002_teste/teste_118.c
@@ -8,15 +8,15 @@ int main() {
   scanf("%d", &quantidade);
 
   for (int i = 0; i < quantidade; ++i) {
-    printf("Digite numero_%d: ", i + 1);
-    scanf("%lf", &arr[i]);
+     printf("Digite numero_%d: ", i + 1);
+     scanf("%lf", &arr[i]);
   }
 
   // armazenando o maior numero para arr[0]
   for (int i = 1; i < quantidade; ++i) {
-    if (arr[0] < arr[i]) {
-      arr[0] = arr[i];
-    }
+     if (arr[0] < arr[i]) {
+        arr[0] = arr[i];
+     }
   }
 
   printf("Maior elemento = %.4lf", arr[0]);
@@ -28,8 +28,8 @@ int main() {
 Digite a quantidade de elementos (1 ateh 100):
 4
 Digite numero_1: 12
-Digite numero_2: 4
-Digite numero_3: 8
-Digite numero_4: 32
-Maior elemento = 32.0000
+Digite numero_2: 3
+Digite numero_3: 40
+Digite numero_4: 15
+Maior elemento = 40.0000
 */
--- 002_teste/teste_119.c
+++ 002_teste/teste_119.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 
 
-float calcularDP(float dados[]); // calcular desvio padrao
+float calcularDP(float dados[]);  // calcular desvio padrao
 
 int main() {
     int indice;
     float meus_dados[10];
     printf("Digite 10 elementos:\n");
+
     for (indice = 0; indice < 10; ++indice)
         scanf("%f", &meus_dados[indice]);
     printf("\nDesvio Padrao = %.6f", calcularDP(meus_dados));
@@ -17,14 +18,16 @@ int main() {
 float calcularDP(float dados[]) {
     float soma = 0.0, media, dv = 0.0;
     int i;
+
     for (i = 0; i < 10; ++i) {
-        soma += dados[i];
+       soma += dados[i];
     }
-    media = soma / 10;
+    media = soma/10;
     for (i = 0; i < 10; ++i) {
-        dv += pow(dados[i] - media, 2);
+       dv += pow(dados[i] - media, 2);
     }
-    return sqrt(dv / 10);
+
+    return sqrt(dv/10);
 }
 
 /*
