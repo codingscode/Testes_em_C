992720246b03a799b89d4e154cbc6c9e2c31e045
codingscode <heronnestg@hotmail.com>
2021-07-12 10:21:40 -0300

    parte 270

--- 002_teste/teste_125.c
+++ 002_teste/teste_125.c
@@ -3,34 +3,34 @@
 
 
 int main() {
-  int n;
-  double *dados;
-  printf("Digitar o numero total de elementos:\n");
-  scanf("%d", &n);
+    int n;
+    double *dados;
+    printf("Digitar o numero total de elementos:\n");
+    scanf("%d", &n);
 
-  // alocando memoria para n elementos
-  dados = (double *) calloc(n, sizeof(double));
-  if (dados == NULL) {
-    printf("Erro!!! memoria nao alocada.");
-    exit(0);
-  }
+    // alocando memoria para n elementos
+    dados = (double *) calloc(n, sizeof(double));
+    if (dados == NULL) {
+       printf("Erro!!! memoria nao alocada.");
+       exit(0);
+    }
 
-  // guardando numeros digitados
-  for (int i = 0; i < n; ++i) {
-    printf("Digite numero%d: ", i + 1);
-    scanf("%lf", dados + i);
-  }
+    // guardando numeros digitados
+    for (int i = 0; i < n; ++i) {
+       printf("Digite numero%d: ", i + 1);
+       scanf("%lf", dados + i);
+    }
 
-  // encontrando o maior numero
-  for (int i = 1; i < n; ++i) {
-    if (*dados < *(dados + i))
-        *dados = *(dados + i);
-  }
-  printf("Maior numero = %.2lf", *dados);
+    // encontrando o maior numero
+    for (int i = 1; i < n; ++i) {
+       if (*dados < *(dados + i))
+          *dados = *(dados + i);
+    }
+    printf("Maior numero = %.2lf", *dados);
 
-  free(dados);
+    free(dados);
 
-  return 0;
+    return 0;
 }
 
 /* 
