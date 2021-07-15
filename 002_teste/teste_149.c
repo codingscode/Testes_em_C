c19ffdb8646944c80c8b5c20e8331d5aec32f18a
codingscode <heronnestg@hotmail.com>
2021-07-15 10:39:40 -0300

    parte 271

--- 002_teste/teste_149.c
+++ 002_teste/teste_149.c
@@ -12,10 +12,10 @@ int main() {
     complexo numero1, numero2, resultado;
 
     printf("Para o primeiro numero complexo:\n");
-    printf("Digite as parte real e imaginaria:\n ");
+    printf("Digite as parte real e imaginaria:\n");
     scanf("%f %f", &numero1.real, &numero1.imaginario);
     printf("\nPara o segundo numero complexo:\n");
-    printf("Digite as parte real e imaginaria:\n ");
+    printf("Digite as parte real e imaginaria:\n");
     scanf("%f %f", &numero2.real, &numero2.imaginario);
 
     resultado = adicionar(numero1, numero2);
@@ -34,10 +34,12 @@ complexo adicionar(complexo n1, complexo n2) {
 /* 
 Para o primeiro numero complexo:
 Digite as parte real e imaginaria:
- 4 7
+4
+7
 
 Para o segundo numero complexo:
 Digite as parte real e imaginaria:
- 1 5
+1
+5
 Soma = 5.0 + 12.0i
 */
