966b0bc495e6ed00cf875df3841241ce771b8ff2
codingscode <heronnestg@hotmail.com>
2021-07-21 09:41:31 -0300

    parte 284

--- aulas/tema1 codigos aula/Exemplo1.c
+++ aulas/tema1 codigos aula/Exemplo1.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-static  int a = 0;  // variável global, alocação estática
+
+static  int a = 0;  // variavel global, alocacao estatica
 
 void incrementa(void) {
-	int b = 0; // variável local, alocação automática
-	static int c = 0; // variável local, alocação estática
+	int b = 0; // variavel local, alocacao automatica
+	static int c = 0; // variavel local, alocacao estatica
 
-	printf ("a: %d, b: %d, c: %d\n", a, b, c);
-	a++ ;
-	b++ ;
-	c++ ;
+	printf("a: %d, b: %d, c: %d\n", a, b, c);
+	a++;
+	b++;
+	c++;
 }
 
 int main(void) {
 	int i;
 
 	for (i = 0; i < 5; i++)
-	incrementa();
+		incrementa();
 
 	system ("pause");
 	return 0;
@@ -28,8 +29,4 @@ a: 1, b: 0, c: 1
 a: 2, b: 0, c: 2
 a: 3, b: 0, c: 3
 a: 4, b: 0, c: 4
-
 */
-
-
-
--- aulas/tema1 codigos aula/Exemplo4.c
+++ aulas/tema1 codigos aula/Exemplo4.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <conio.h>
+
+
 int main(void) {
-      //v_num é a variável que será apontada pelo ponteiro
+      //v_num eh a variavel que serah apontada pelo ponteiro
       int v_num = 10;
       
-      //declaração de variável ponteiro
+      //declaracao de variavel ponteiro
       int *ptr;
       
-      //atribuindo o endereço da variável v_num ao ponteiro
+      //atribuindo o endereco da variavel v_num ao ponteiro
       ptr = &v_num;
       
       printf("Utilizando ponteiros\n\n");
-      printf ("Conteudo da variavel v_num: %d\n", v_num);
-      printf ("Endereço da variavel v_num: %x \n", &v_num);
-      printf ("Conteudo da variavel ponteiro ptr: %x", ptr);
+      printf("Conteudo da variavel v_num: %d\n", v_num);
+      printf("Endereco da variavel v_num: %x \n", &v_num);
+      printf("Conteudo da variavel ponteiro ptr: %x", ptr);
       
       getch();
       return(0);
@@ -23,8 +25,7 @@ int main(void) {
 Utilizando ponteiros
 
 Conteudo da variavel v_num: 10
-Endereþo da variavel v_num: 62fe14
+Endereco da variavel v_num: 62fe14
 Conteudo da variavel ponteiro ptr: 62fe14
 
 */
-
