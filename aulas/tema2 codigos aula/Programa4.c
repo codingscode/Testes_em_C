0d1df57ccdf5582f64dca8e6ce8b7731cc0c8320
codingscode <heronnestg@hotmail.com>
2021-07-23 11:06:00 -0300

    parte 291

--- aulas/tema2 codigos aula/Programa4.c
+++ aulas/tema2 codigos aula/Programa4.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(int argc, char *argv[]){
-  int i;
-  scanf("%i", &i);
-  printf("%i\n", i);
-  return (0);
+   int i;
+
+   scanf("%i", &i);
+   printf("%i\n", i);
+   
+   return 0;
 }
 
 /* 
