18eb1eb51d340f2c0fe04146be51913535950266
codingscode <heronnestg@hotmail.com>
2021-07-24 18:59:10 -0300

    parte 294

--- aulas/tema2 codigos aula/Programa8.c
+++ aulas/tema2 codigos aula/Programa8.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
+
 void v_iniciacao(int *vet, int n) {
      int i;
 
-     for (i=0; i<n; i++)
-          vet[i]=0;        
+     for (i = 0; i < n; i++)
+        vet[i] = 0;        
 }
 
 void v_imprime(int *vet,  int n) { 
      int i;  
 
-     for (i=0;  i < n;  i++)      
-          printf(" %d - ",vet[i]);       
+     for (i = 0;  i < n; i++)      
+        printf(" %d - ", vet[i]);       
      printf("\n");  
 }
 
@@ -24,15 +25,15 @@ int main(void) {
      v_imprime(vet, 10);   
 
      // Mudando valores de vet.
-     for (i=0; i<10; i++) {
-          vet[i]=i;
+     for (i = 0; i < 10; i++) {
+        vet[i] = i;
      }
 
      // Impressao apos atribuicao.   
      printf("Impressao do vetor apos a atribuicao."); 
      v_imprime(vet, 10);
      
-     return(0);
+     return 0;
 }
 
 /*
