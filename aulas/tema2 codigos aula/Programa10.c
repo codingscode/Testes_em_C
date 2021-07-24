c1db34dc9f5bd33406ba03e232aaaaaad24a8648
codingscode <heronnestg@hotmail.com>
2021-07-24 19:02:25 -0300

    parte 295

--- aulas/tema2 codigos aula/Programa10.c
+++ aulas/tema2 codigos aula/Programa10.c
@@ -1,6 +1,7 @@
 //Programa 10 - soma dois numeros
 #include <stdio.h> 
 
+
 int num3;
 
 int main () {
@@ -8,6 +9,7 @@ int main () {
     num1 = 10;
     num2 = 20;
     num3 = num1 + num2; 
+    
     printf("%d + %d = %d", num1, num2, num3); 
 
     return 0;
--- aulas/tema2 codigos aula/Programa9.c
+++ aulas/tema2 codigos aula/Programa9.c
@@ -7,6 +7,7 @@ int main () {
     num1 = 10;
     num2 = 20;
     num3 = num1 + num2; 
+
     printf("%d + %d = %d", num1, num2, num3); 
     return 0;
 }
