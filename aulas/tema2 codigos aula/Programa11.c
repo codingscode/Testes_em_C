32100eebb5b3d4e412ed8c41b78dcde9c5fb3b3f
codingscode <heronnestg@hotmail.com>
2021-07-26 09:29:42 -0300

    parte 296

--- aulas/tema2 codigos aula/Programa11.c
+++ aulas/tema2 codigos aula/Programa11.c
@@ -1,12 +1,16 @@
-//Programa imprime o valor da vari�vel num
+//Programa imprime o valor da variavel num
 #include <stdio.h> 
+
 
 int num;
 num = 20;
 
 int main () {
-    int num;  // experimentar comentar  
-    num = 10; // experimentar comentar
+    int num;  // so este comentado -> O valor da variavel num: 10
+    num = 10; // so este comentado -> O valor da variavel num: 0
+
+    //sem comentar-> O valor da variavel num: 10
+    
     printf("O valor da variavel num: %d", num); 
     return 0;
 }
---
+++ aulas/tema2 codigos aula/Programa11_outro.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+
+int main () {
+    int valor;
+
+    printf("variavel valor: %d", valor);
+
+    return 0;
+}
+
+/* 
+variavel valor: 0
+*/
