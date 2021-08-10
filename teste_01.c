c1f02b82aef773fed45cf9a60aecbc3b03b15846
codingscode <heronnestg@hotmail.com>
2021-08-10 17:43:59 -0300

    parte 301

---
+++ teste_01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main() {
+    int numero;
+
+    printf("digite um numero:\n");
+    scanf("%d", &numero);
+
+    for (int i = 0; i < numero; i++) {
+        printf("%d \n", i);
+    }
+
+
+    return 0;
+}
+
+
+
+
+
+
+
