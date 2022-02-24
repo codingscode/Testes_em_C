80d53e93794bcdbe63d4098a8b1ff8960e943a34
codingscode <heronnestg@hotmail.com>
2022-02-24 20:57:30 -0300

    parte 306

---
+++ aulas/teste.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <math.h>
+
+
+
+int main(void) {
+   int j;
+    
+   for (j = 0; j < 8; j++) {
+      printf("valor: %d\n", j);
+   }
+
+		 
+   return 0;
+}
+
+/* 
+valor: 0
+valor: 1
+valor: 2
+valor: 3
+valor: 4
+valor: 5
+valor: 6
+valor: 7
+*/
